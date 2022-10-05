7308717c642c898c36b06c01cbb25e7e5450b375
Victoriia <nosareva.vs@gmail.com>
2022-10-05 16:33:37 +0300

    update components

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,15 +1,60 @@
 <template>
-    <div>
-
-    </div>
+    <form @submit.prevent>
+        <h4>Создание поста</h4>
+        <input v-model="post.title" class="input" type="text" placeholder="Название">
+        <input v-model="post.body" class="input" type="text" placeholder="Описание">
+        <button @click="createPost" class="btnPost">Создать пост</button>
+    </form>
 </template>
 
 <script>
-    export default {
-        
+export default {
+    data() {
+        return {
+            post: {
+                title: '',
+                body: '',
+            }
+        }
+    },
+
+    methods: {
+        createPost() {
+            this.post.id = Date.now()
+            this.$emit('create', this.post)
+            this.post = {
+                title: '',
+                body: '',
+            }
+        }
     }
+}
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+form {
+    display: flex;
+    flex-direction: column;
+}
 
+.input {
+    width: 100%;
+    border: 1px solid darkcyan;
+    padding: 8px 16px;
+    margin-top: 8px;
+}
+
+.btnPost {
+    max-width: 240px;
+    padding: 8px 16px;
+    margin-top: 8px;
+    align-self: flex-end;
+    color: white;
+    background-color: darkcyan;
+    border: none;
+}
+
+.btnPost:hover {
+    background-color: rgb(0, 201, 201);
+}
 </style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,15 +1,25 @@
 <template>
-    <div>
-
+    <div class="post" v-for="post in posts">
+        <div><strong>Название: </strong>{{post.title}}</div>
+        <div><strong>Описание: </strong>{{post.body}}</div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+export default {
+    props: {
+        posts: {
+            type: Array,
+            required: true,
+        }
     }
+}
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped> /* lang="scss" scoped */
+.post {
+    padding: 16px;
+    border: 2px solid darkcyan;
+    margin-top: 16px;
+}
 </style>
